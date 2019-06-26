0751beace5adf6d4c637a9a719a66f29bd225fca
Jhony Luiz de Almeida <2943590@univali.br>
2019-06-25 22:12:29 -0300

    tudo desenvolvido em sala

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table class="table">
+      <tr>
+        <th>ID</th>
+        <th>Equipamento</th>
+        <th>Descrição</th>
+      </tr>
+      <tr v-for="chamado in chamados" :key="chamado.id">
+        <td>{{chamado.id}}</td>
+        <td>{{chamado.equipamento}}</td>
+        <td>{{chamado.descricao}}</td>
+      </tr>
+    </table>
+    <br />
+    <form @submit.prevent="salvar">
+      Equipamento: <input v-model="chamado.equipamento" type="text" />
+      Descricao: <input v-model="chamado.descricao" type="text" />
+      <button type="submit">Salvar</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: () => ({
+    chamados: [],
+    chamado: {
+      equipamento: '',
+      descricao: ''
+    }
+  }),
+  created() {
+    fetch('http://localhost:8000/chamados')
+        .then(response => response.json())
+        .then(chamados => this.chamados = chamados)
+  },
+  methods: {
+    salvar() {
+      const headers = new Headers();
+      headers.set('Content-type', 'application/json')
+      fetch('http://localhost:8000/chamados', { 
+            method: 'POST',
+            headers: headers,
+            mode: 'cors',
+            body: JSON.stringify(this.chamado)  
+          })
+          .then(response => response.json())
+          .then(chamado => this.chamados.push(chamado))
+    }
   }
 }
 </script>
